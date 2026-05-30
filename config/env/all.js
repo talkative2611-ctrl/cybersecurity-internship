57e8966382c1e73c9299baffddff47281b1003b7
talkative2611-ctrl <talkative2611@gmail.com>
2026-05-30 15:39:24 +0100

    Week 1: Security Assessment - NodeGoat setup

--- config/env/all.js
+++ config/env/all.js
@@ -1,6 +1,6 @@
 // default app configuration
 const port = process.env.PORT || 4000;
-let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
+let db = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nodegoat";
 
 module.exports = {
     port,
